00b629c327072b7abb6ae1cf7d5bfe76b9fdf575
Deword <Richard@10.0.0.5>
2020-10-15 22:43:48 +0300

    working with interface

--- ProPets_lostfound_microservice_0.0.1/src/telran/lostfound/service/interfaces/ILostFoundManagement.java
+++ ProPets_lostfound_microservice_0.0.1/src/telran/lostfound/service/interfaces/ILostFoundManagement.java
@@ -1,14 +1,31 @@
 package telran.lostfound.service.interfaces;
 
 import telran.lostfound.api.LostFoundDto;
+import telran.lostfound.api.PostDto;
 import telran.lostfound.api.ResponseLostFoundDto;
 import telran.lostfound.api.GetPostsResponseDto;
 
 public interface ILostFoundManagement {
 	
-	ResponseLostFoundDto newLostPet(LostFoundDto dto);
-	ResponseLostFoundDto newFoundPet(LostFoundDto dto);
-	GetPostsResponseDto getPostsOfLostPets(int items, int currentPage); //fix it later
+	ResponseLostFoundDto newLostPet(LostFoundDto dto); //get ret x-token
+	ResponseLostFoundDto newFoundPet(LostFoundDto dto); //get ret
+	GetPostsResponseDto getPostsOfLostPets(int items, int currentPage); // get ret
+	GetPostsResponseDto getPostsOfFoundPets(int items, int currentPage);  //get ret
 	
-
+	GetPostsResponseDto searchInfoOfFound( //get ret
+//			dto from Imaga,
+			int items, int currentPage);
+	GetPostsResponseDto searchInfoOfLost( //get ret
+//			dto from Imaga,
+			int items, int currentPage);
+	
+	PostDto postById(String postID); //ret get
+	
+	//update post get ret
+	//delete post by id get ret
+	
+	//tags and colors CONNECT TO API 
+	
+	//get user data ??? from another service: input _ID_ posts, out content posts by ID.
+			
 }
